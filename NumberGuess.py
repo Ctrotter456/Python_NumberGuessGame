3babff6bb15e76f5b2b5ca318cf5c2f21c78e011
Ctrotter456 <Christopher.trotter@hotmail.com>
2018-02-15 02:37:59 +0000

    Update NumberGuess.py

--- NumberGuess.py
+++ NumberGuess.py
@@ -3,8 +3,7 @@ import random
 print("Guess the Number")
 userinput = input("Enter a number between 1 & 100\n")
 
-for x in range(1):
-    generatednumber = random.randint(0, 101)
+generatednumber = random.randint(0, 101)
 
 if userinput == generatednumber:
     print("Correct, Number =" + generatednumber)
